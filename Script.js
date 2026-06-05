23611d75c5755e4319d2975bf646e4418b176ed9
rithikapremsingh-cpu <rithikapremsingh@gmail.com>
2026-06-05 11:00:44 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+alert("Welcome to Rithika Singh's Portfolio Website!");
+
+console.log("Portfolio Website Loaded Successfully");
+
+function showMessage() {
+    alert("Thank you for visiting my portfolio!");
+}
